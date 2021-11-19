8ff27c63760226958532d497162e41407bf5de0c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-11-19 23:53:30 +0900

    ᕦ(ò_óˇ)ᕤ Imrove TextStyle

--- carrot/lib/start/intro_page.dart
+++ carrot/lib/start/intro_page.dart
@@ -11,19 +11,34 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("당근마켓"),
-        ExtendedImage.asset("assets/imgs/carrot_intro.png"),
-        Text("우리동네 중고 직거래 마켓"),
-        Text("당근마켓은 동네 직거래 마켓이예요\n내 동네를 설정하고 시작해보세요."),
-        TextButton(
-            onPressed: onButtonClick,
-            child:
-                Text("내 동네 설정하고 시작하기", style: TextStyle(color: Colors.white)),
-            style:
-                TextButton.styleFrom(backgroundColor: Colors.lightBlueAccent))
-      ],
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "당근마켓",
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.redAccent,
+                fontWeight: FontWeight.bold),
+          ),
+          ExtendedImage.asset("assets/imgs/carrot_intro.png"),
+          Text(
+            "우리동네 중고 직거래 마켓",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "당근마켓은 동네 직거래 마켓이예요\n내 동네를 설정하고 시작해보세요.",
+            style: TextStyle(fontSize: 16),
+          ),
+          TextButton(
+              onPressed: onButtonClick,
+              child:
+                  Text("내 동네 설정하고 시작하기", style: TextStyle(color: Colors.white)),
+              style:
+                  TextButton.styleFrom(backgroundColor: Colors.lightBlueAccent))
+        ],
+      ),
     );
   }
 }
