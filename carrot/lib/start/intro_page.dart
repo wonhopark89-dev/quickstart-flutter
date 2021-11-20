9dea2cfda55f8530413f0f36704767a80b5d398c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-11-20 23:39:34 +0900

    ᕦ(ò_óˇ)ᕤ Add Padding and Alignment

--- carrot/lib/start/intro_page.dart
+++ carrot/lib/start/intro_page.dart
@@ -12,32 +12,40 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            "당근마켓",
-            style: TextStyle(
-                fontSize: 30,
-                color: Colors.redAccent,
-                fontWeight: FontWeight.bold),
-          ),
-          ExtendedImage.asset("assets/imgs/carrot_intro.png"),
-          Text(
-            "우리동네 중고 직거래 마켓",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "당근마켓은 동네 직거래 마켓이예요\n내 동네를 설정하고 시작해보세요.",
-            style: TextStyle(fontSize: 16),
-          ),
-          TextButton(
-              onPressed: onButtonClick,
-              child:
-                  Text("내 동네 설정하고 시작하기", style: TextStyle(color: Colors.white)),
-              style:
-                  TextButton.styleFrom(backgroundColor: Colors.lightBlueAccent))
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              "당근마켓",
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.redAccent,
+                  fontWeight: FontWeight.bold),
+            ),
+            ExtendedImage.asset("assets/imgs/carrot_intro.png"),
+            Text(
+              "우리동네 중고 직거래 마켓",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "당근마켓은 동네 직거래 마켓이예요\n내 동네를 설정하고 시작해보세요.",
+              style: TextStyle(fontSize: 16),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextButton(
+                    onPressed: onButtonClick,
+                    child: Text("내 동네 설정하고 시작하기",
+                        style: TextStyle(color: Colors.white)),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.lightBlueAccent)),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
