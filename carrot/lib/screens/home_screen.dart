33b215ce726095534fcdd0dfdb004070714d74eb
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-08 01:47:05 +0900

    ᕦ(ò_óˇ)ᕤ Add AppBar and Cupertino Icons

--- carrot/lib/screens/home_screen.dart
+++ carrot/lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carrot/states/user_provider.dart';
 import 'package:provider/provider.dart';
@@ -9,12 +10,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+      centerTitle: false,
+      title: Text("우리동네", style: Theme.of(context).appBarTheme.titleTextStyle),
       actions: [
         IconButton(
             onPressed: () {
               context.read<UserProvider>().setUserAuth(false);
             },
-            icon: Icon(Icons.logout))
+            icon: Icon(CupertinoIcons.nosign)),
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.text_justify))
       ],
     ));
   }
