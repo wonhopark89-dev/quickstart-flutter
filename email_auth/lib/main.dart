2563d195b2e60d9ade05ca5dd810ed183acc8cc5
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-04-16 23:57:33 +0900

    :fire: firebase_core wrapping

--- email_auth/lib/main.dart
+++ email_auth/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:email_auth/pages/auth_page.dart';
@@ -5,10 +6,34 @@ import 'package:email_auth/pages/my_home.dart';
 import 'package:email_auth/provider/page_notifier.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("문제가 있습니다. 다시 시도해주세요"),
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const MyApp();
+          }
+
+          return const CircularProgressIndicator();
+        });
+  }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
