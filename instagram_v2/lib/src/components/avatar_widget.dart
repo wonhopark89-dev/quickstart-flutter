3368e096a431b709e45ca4107b7949d95d5e2e38
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-04-24 02:46:34 +0900

    :camera_flash: avatar with cached image

---
+++ instagram_v2/lib/src/components/avatar_widget.dart
@@ -0,0 +1,66 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+enum AvatarType { TYPE1, TYPE2, TYPE3 }
+
+class AvatarWidget extends StatelessWidget {
+  final bool? hasStory;
+  final String thumbPath;
+  final String? nickname;
+  final AvatarType type;
+  final double? size;
+
+  const AvatarWidget(
+      {Key? key,
+      this.hasStory,
+      required this.thumbPath,
+      this.nickname,
+      required this.type,
+      this.size = 60})
+      : super(key: key);
+
+  Widget type1Widget() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.orange, Colors.pink, Colors.black]),
+          shape: BoxShape.circle),
+      child: type2Widget(),
+    );
+  }
+
+  Widget type2Widget() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      decoration:
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.white),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(size!),
+        child: SizedBox(
+          width: size,
+          height: size,
+          child: CachedNetworkImage(
+            imageUrl: thumbPath,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (type) {
+      case AvatarType.TYPE1:
+        return type1Widget();
+      case AvatarType.TYPE2:
+        return type2Widget();
+      case AvatarType.TYPE3:
+        return Container();
+    }
+  }
+}
--- instagram_v2/lib/src/pages/home.dart
+++ instagram_v2/lib/src/pages/home.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_v2/src/app.dart';
+import 'package:instagram_v2/src/components/avatar_widget.dart';
 import 'package:instagram_v2/src/components/image_data.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
+  Widget _myStory() {
+    return Stack(
+      children: [
+        const AvatarWidget(
+            size: 65,
+            thumbPath:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxzfjLcrtvrt4qwdPFoVYbshPcWLhh8Xve2w&usqp=CAU",
+            type: AvatarType.TYPE2),
+        Positioned(
+          right: 5,
+          bottom: 0,
+          child: Container(
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.blueAccent,
+              border: Border.all(color: Colors.white, width: 1),
+            ),
+            child: const Center(
+              child: Text(
+                "+",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    height: 1.1),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget _storyBoardList() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: List.generate(
-          50,
-          (index) => Container(
-            width: 60,
-            height: 60,
-            decoration:
-                const BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
+        children: [
+          const SizedBox(
+            width: 10,
           ),
-        ),
+          _myStory(),
+          ...List.generate(
+            50,
+            (index) => const AvatarWidget(
+                thumbPath:
+                    "http://m.ebichu.co.kr/web/product/medium/20200227/9dc69754e4800cf4332225fb6fcaf37b.png",
+                type: AvatarType.TYPE1),
+          ),
+        ],
       ),
     );
   }
