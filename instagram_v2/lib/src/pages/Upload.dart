b8c53c95978da56d7085861d903b7bf47ec1dbd4
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-05-01 03:22:50 +0900

    :camera_flash: photo from albums with permission

--- instagram_v2/lib/src/pages/Upload.dart
+++ instagram_v2/lib/src/pages/Upload.dart
@@ -1,12 +1,87 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:instagram_v2/src/components/image_data.dart';
+import 'dart:typed_data';
 
-class Upload extends StatelessWidget {
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+import 'package:instagram_v2/src/components/image_data.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+class Upload extends StatefulWidget {
   const Upload({Key? key}) : super(key: key);
 
+  @override
+  State<Upload> createState() => _UploadState();
+}
+
+class _UploadState extends State<Upload> {
+  var albums = <AssetPathEntity>[];
+  var imageList = <AssetEntity>[];
+  var headerTitle = "";
+  AssetEntity? selectedImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPhotos();
+  }
+
+  void _loadPhotos() async {
+    var result = await PhotoManager.requestPermissionExtend();
+    if (result.isAuth) {
+      albums = await PhotoManager.getAssetPathList(
+        type: RequestType.image,
+        filterOption: FilterOptionGroup(
+            imageOption: const FilterOption(
+              sizeConstraint: SizeConstraint(minHeight: 100, minWidth: 100),
+            ),
+            orders: const [
+              // 사진이 최신순으로
+              OrderOption(type: OrderOptionType.createDate, asc: false)
+            ]),
+      );
+      _loadData();
+    } else {
+      // 권한 요청
+    }
+  }
+
+  void _loadData() async {
+    headerTitle = albums.first.name;
+    await _pagingPhotos();
+    update();
+    // setState(() {
+    //   headerTitle = albums.first.name;
+    // });
+    // print(albums.first.name);
+  }
+
+  Future<void> _pagingPhotos() async {
+    var photos =
+        await albums.first.getAssetListPaged(page: 0, size: 30); // 처음에 30개
+    imageList.addAll(photos);
+    selectedImage = imageList.first;
+  }
+
+  void update() => setState(() {});
+
   Widget _imagePreview() {
-    return Container(width: Get.width, height: Get.width, color: Colors.grey);
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      width: width,
+      height: width,
+      color: Colors.grey,
+      child: selectedImage == null
+          ? Container()
+          : _photoWidget(
+              selectedImage!,
+              width.toInt(),
+              builder: (data) {
+                return Image.memory(
+                  data,
+                  fit: BoxFit.cover,
+                );
+              },
+            ),
+    );
   }
 
   Widget _header() {
@@ -18,10 +93,10 @@ class Upload extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(5),
             child: Row(
-              children: const [
-                Text("갤러리",
-                    style: TextStyle(color: Colors.black, fontSize: 18)),
-                Icon(Icons.arrow_drop_down),
+              children: [
+                Text(headerTitle,
+                    style: const TextStyle(color: Colors.black, fontSize: 18)),
+                const Icon(Icons.arrow_drop_down),
               ],
             ),
           ),
@@ -70,11 +145,37 @@ class Upload extends StatelessWidget {
         mainAxisSpacing: 1,
         crossAxisSpacing: 1,
       ),
-      itemCount: 100,
+      itemCount: imageList.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          color: Colors.amber,
+        return _photoWidget(
+          imageList[index],
+          200,
+          builder: (data) {
+            return Opacity(
+              opacity: imageList[index] == selectedImage ? 0.3 : 1,
+              child: Image.memory(
+                data,
+                fit: BoxFit.cover,
+              ),
+            );
+          },
         );
+      },
+    );
+  }
+
+  Widget _photoWidget(AssetEntity asset, int size,
+      {required Widget Function(Uint8List) builder}) {
+    // 파일추출
+    // asset.thumbnailData(200, 200); // Future 로 감싸져있음
+    return FutureBuilder(
+      future: asset.thumbnailDataWithSize(ThumbnailSize(size, size)),
+      builder: (_, AsyncSnapshot<Uint8List?> snapshot) {
+        if (snapshot.hasData) {
+          return builder(snapshot.data!);
+        } else {
+          return Container();
+        }
       },
     );
   }
