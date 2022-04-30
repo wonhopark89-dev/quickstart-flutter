fb5da24beb81459ad8e7c21eeb1949372af9904d
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-04-30 15:09:17 +0900

    :camera_flash: search app bar

--- instagram_v2/lib/src/app.dart
+++ instagram_v2/lib/src/app.dart
@@ -4,6 +4,7 @@ import 'package:instagram_v2/src/components/image_data.dart';
 import 'package:instagram_v2/src/controller/BottomNavController.dart';
 import 'package:instagram_v2/src/pages/home.dart';
 import 'package:instagram_v2/src/pages/search.dart';
+import 'package:instagram_v2/src/pages/upload.dart';
 
 class App extends GetView<BottomNavController> {
   const App({Key? key}) : super(key: key);
@@ -26,11 +27,7 @@ class App extends GetView<BottomNavController> {
                   );
                 },
               ),
-              Container(
-                child: const Center(
-                  child: Text("UPLOAD"),
-                ),
-              ),
+              const Upload(),
               Container(
                 child: const Center(
                   child: Text("ACTIVITY"),
--- instagram_v2/lib/src/pages/Upload.dart
+++ instagram_v2/lib/src/pages/Upload.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_v2/src/components/image_data.dart';
 
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.amberAccent);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+            onTap: () {},
+            child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: ImageData(IconsPath.closeImage))),
+        title: const Text(
+          "New Post",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
+        ),
+        actions: [
+          GestureDetector(
+              onTap: () {},
+              child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: ImageData(
+                    IconsPath.nextImage,
+                    width: 50,
+                  ))),
+        ],
+      ),
+    );
   }
 }
