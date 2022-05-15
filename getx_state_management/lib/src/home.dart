8a975de77f1aef01f439654bb6f1bf0cd59b331c
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-05-15 23:53:20 +0900

    :green_heart: getx basic setting

---
+++ getx_state_management/lib/src/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("라우트 관리 홈"),
+      ),
+      body: Center(
+          child: Column(
+        children: const [],
+      )),
+    );
+  }
+}
