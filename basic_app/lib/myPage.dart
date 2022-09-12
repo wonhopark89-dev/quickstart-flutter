02b11693f4244928fa840e429adaf800c9c35c4f
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-09-13 03:50:09 +0900

    drawer

--- basic_app/lib/myPage.dart
+++ basic_app/lib/myPage.dart
@@ -7,29 +7,95 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('appbar icon menu'),
+        title: const Text('appbar icon menu'),
         centerTitle: true,
         elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            print('menu button is clicked');
-          },
-          icon: Icon(Icons.menu),
-        ),
+        // leading: IconButton(
+        //   onPressed: () {
+        //     print('menu button is clicked');
+        //   },
+        //   icon: const Icon(Icons.menu),
+        // ),
         actions: [
           IconButton(
             onPressed: () {
               print('shopping cart is clicked');
             },
-            icon: Icon(Icons.shopping_cart),
+            icon: const Icon(Icons.shopping_cart),
           ),
           IconButton(
             onPressed: () {
               print('search is clicked');
             },
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: AssetImage('assets/ebichu_1.jpeg'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: const [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/ebichu_1.jpeg'),
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage('assets/ebichu_1.jpeg'),
+                // )
+              ],
+              accountName: const Text('Ebichu'),
+              accountEmail: const Text('edi@chu'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.blueGrey[800],
+              ),
+              title: const Text('Home'),
+              trailing: const Icon(Icons.add),
+              onTap: () {
+                print('home is clicked');
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.blueGrey[800],
+              ),
+              title: const Text('Setting'),
+              trailing: const Icon(Icons.add),
+              onTap: () {
+                print('Setting is clicked');
+              },
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.blueGrey[800],
+              ),
+              title: const Text('Q&A'),
+              trailing: const Icon(Icons.add),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+            )
+          ],
+        ),
       ),
     );
   }
