4631050b9b7fb92a1291b697dc1858d03945e1f0
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-09-12 19:42:36 +0900

    basic material app

--- basic_app/lib/main.dart
+++ basic_app/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'First App',
       theme: ThemeData(
         primarySwatch: Colors.orange,
@@ -23,19 +24,117 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: const Text('펭수'),
+        title: const Text('에비츄'),
         centerTitle: true,
-        backgroundColor: Colors.orangeAccent,
+        backgroundColor: Colors.orangeAccent[200],
         elevation: 0,
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello'),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ebichu_2.gif'),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 30, // 위아래 합친 공간
+              color: Colors.grey[800],
+              thickness: 1,
+            ),
+            const Text(
+              'Name',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              'AAA',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'AAA POWER LEVEL',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.5,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              '30',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'using lightsaber',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1.0),
+                )
+              ],
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'face hero tattoo',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1.0),
+                )
+              ],
+            ),
+            Row(
+              children: const [
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'fire flames',
+                  style: TextStyle(fontSize: 16, letterSpacing: 1.0),
+                )
+              ],
+            ),
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ebichu_1.jpeg'),
+                backgroundColor: Colors.blueGrey,
+                radius: 40,
+              ),
+            )
           ],
         ),
       ),
