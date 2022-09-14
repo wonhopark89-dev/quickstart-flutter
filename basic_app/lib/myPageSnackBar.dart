beb559a00a161f4a4886c6fcddbbcbb9344cdc1b
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-09-15 03:20:17 +0900

    buildContext 재 생성없이 사용방법

--- basic_app/lib/myPageSnackBar.dart
+++ basic_app/lib/myPageSnackBar.dart
@@ -10,25 +10,55 @@ class MyPageSnackBar extends StatelessWidget {
         title: const Text('Snack Bar'),
         centerTitle: true,
       ),
-      body: Builder(
-        builder: (BuildContext ctx) {
-          return Center(
-            child: ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(ctx).showSnackBar(
-                  const SnackBar(
-                    content: Text("Hello"),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(primary: Colors.red),
-              child: const Text(
-                'Show me',
-                style: TextStyle(color: Colors.white),
+      // body: Builder(
+      //   builder: (BuildContext ctx) {
+      //     return Center(
+      //       child: ElevatedButton(
+      //         onPressed: () {
+      //           ScaffoldMessenger.of(ctx).showSnackBar(
+      //             const SnackBar(
+      //               content: Text("Hello"),
+      //             ),
+      //           );
+      //         },
+      //         style: ElevatedButton.styleFrom(primary: Colors.red),
+      //         child: const Text(
+      //           'Show me',
+      //           style: TextStyle(color: Colors.white),
+      //         ),
+      //       ),
+      //     );
+      //   },
+      // ),
+      body: const MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.teal,
+              duration: Duration(milliseconds: 1000),
+              content: Text(
+                "Hello",
+                textAlign: TextAlign.center,
               ),
             ),
           );
         },
+        style: ElevatedButton.styleFrom(primary: Colors.red),
+        child: const Text(
+          'Show me',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
