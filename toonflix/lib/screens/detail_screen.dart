92b61b14685d6940d8e09ee249432077c5d22914
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-12-11 15:56:17 +0900

    [toonflix] hero widget

--- toonflix/lib/screens/detail_screen.dart
+++ toonflix/lib/screens/detail_screen.dart
@@ -25,20 +25,23 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 10,
-                      offset: const Offset(10, 10),
-                      color: Colors.orange.withOpacity(0.5),
-                    )
-                  ],
+              Hero(
+                tag: webtoon.id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 10,
+                        offset: const Offset(10, 10),
+                        color: Colors.orange.withOpacity(0.5),
+                      )
+                    ],
+                  ),
+                  child: Image.network(webtoon.thumb),
                 ),
-                child: Image.network(webtoon.thumb),
               ),
             ],
           ),
